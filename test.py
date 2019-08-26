198315fe0498005eadb56acbbcc711294bf9b079
Dao Quang Huy <daoquanghuy.uet.140598@gmail.com>
2019-08-26 22:55:13 +0700

    add rgb image

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ import cv2
 import matplotlib.pyplot as plt
 img = cv2.imread("BZ7LGZSXCFHOXAN2XCTAM4QRPQ.jpg")
 grayscale = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+rgb = cv2.cvtColor(img,cv2.COLOR_BAYER_BG2RGB)
 plt.imshow(img[:,:,::-1])
 plt.show()

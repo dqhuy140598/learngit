83cada0e917bf8fa1ab740c825efb01fb7e90f62
Dao Quang Huy <daoquanghuy.uet.140598@gmail.com>
2019-08-26 22:52:45 +0700

    change test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import cv2
 import matplotlib.pyplot as plt
 img = cv2.imread("BZ7LGZSXCFHOXAN2XCTAM4QRPQ.jpg")
-
+grayscale = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 plt.imshow(img[:,:,::-1])
 plt.show()

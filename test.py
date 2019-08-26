4d91de58a97cf8821f8634c963e77d5363f4aa82
Dao Quang Huy <daoquanghuy.uet.140598@gmail.com>
2019-08-26 23:18:13 +0700

    numpy

--- test.py
+++ test.py
@@ -3,5 +3,6 @@ import matplotlib.pyplot as plt
 img = cv2.imread("BZ7LGZSXCFHOXAN2XCTAM4QRPQ.jpg")
 grayscale = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 rgb = cv2.cvtColor(img,cv2.COLOR_BAYER_BG2RGB)
+import numpy as np
 plt.imshow(img[:,:,::-1])
 plt.show()
